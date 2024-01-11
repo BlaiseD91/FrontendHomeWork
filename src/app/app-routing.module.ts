028e1a94c868f5f5c5f5e67e59094956eb2ea67e
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-01-11 22:34:40 +0100

    bookings component added, router-outlet placed, bookings rout set, bookings routerLink added to its button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookingsComponent } from './bookings/bookings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"bookings", component: BookingsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
