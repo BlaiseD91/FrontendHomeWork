b31d81dc1482a3812cce2e72b59106f31513d4b4
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-01-12 10:51:36 +0100

    table added to bookings component, show data but not ordered

--- src/app/bookings/bookings.component.ts
+++ src/app/bookings/bookings.component.ts
@@ -9,13 +9,13 @@ import { BaseService } from '../base.service';
 export class BookingsComponent {
 
   bookings:any;
-
+  tableHeaders = ["Dátum", "Név", "Fő", "Cím", "Iránytószám"]
 
   constructor(private baseService:BaseService){
     this.baseService.getBookings().subscribe(
       {
-        next: (response) => {
-          this.bookings = response
+        next: (res) => {
+          this.bookings = res
         },
         error: (err) => console.log(err)
       }
