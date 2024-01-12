b234d79eb637174b8b148240ec34b4339a5533c0
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-01-12 11:00:49 +0100

    ujfoglalas route set and routerlink added to the button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookingsComponent } from './bookings/bookings.component';
+import { NewBookingComponent } from './new-booking/new-booking.component';
 
 const routes: Routes = [
-  { path:"foglalasok", component: BookingsComponent}
+  { path:"foglalasok", component: BookingsComponent},
+  { path:"ujfoglalas", component: NewBookingComponent}
 ];
 
 @NgModule({
