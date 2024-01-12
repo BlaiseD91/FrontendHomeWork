c1e00d897d9858c0601e9a3b1faa190afcdaec27
Székely Balázs Csaba <szekelyb91@gmail.com>
2024-01-12 11:41:33 +0100

    date field show always todays date and this is the min value of it

--- src/app/new-booking/new-booking.component.ts
+++ src/app/new-booking/new-booking.component.ts
@@ -1,10 +1,19 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-new-booking',
   templateUrl: './new-booking.component.html',
-  styleUrls: ['./new-booking.component.css']
+  styleUrls: ['./new-booking.component.css'],
+  providers: [DatePipe]
 })
 export class NewBookingComponent {
 
+  date:any
+
+  constructor(private datePipe: DatePipe) {
+    this.date = this.datePipe.transform(new Date(), 'yyyy-MM-dd')
+
+    console.log(this.date)
+  }
 }
